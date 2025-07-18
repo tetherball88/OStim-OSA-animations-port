213c2bbd65f48a9e24749f41ad87f67880e02bf7
tetherball88 <dwss1988@protonmail.com>
2025-07-17 22:07:53 -0400

    bad boys

--- removeBehaviorFiles.bat
+++ removeBehaviorFiles.bat
@@ -38,6 +38,16 @@ if exist "%DEL_PATH%" (
 )
 echo.
 
+REM --- DELETE FOLDER IF EXISTS ---
+set "DEL_PATH=%ROOT%\OSexPack Bad Boys of Skyrim\meshes\actors\character\behaviors"
+if exist "%DEL_PATH%" (
+    echo Found folder to delete: "%DEL_PATH%"
+    rmdir /s /q "%DEL_PATH%"
+    echo Deleted folder: "%DEL_PATH%"
+) else (
+    echo Folder not found: "%DEL_PATH%" (nothing to delete)
+)
+echo.
 
 
 echo Done.
